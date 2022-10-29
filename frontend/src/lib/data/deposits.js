1bfe71dcc9e035c0431b0c22d1b6394cd8e9038f
Paula Carneiro <paulacarneiro215@gmail.com>
2022-10-29 23:37:05 +0100

    Add deposits table (#13)

---
+++ frontend/src/lib/data/deposits.js
@@ -0,0 +1,20 @@
+export const deposits = [
+  {
+    timestamp: 1661794197000,
+    transaction_hash:
+      "0xe3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+    amount: 0.1,
+  },
+  {
+    timestamp: 1664472597000,
+    transaction_hash:
+      "0x05a9bf223fedf80a9d0da5f73f5c191a665bf4a0a4a3e608f2f9e7d5ff23959c",
+    amount: 0.1,
+  },
+  {
+    timestamp: 1667064597000,
+    transaction_hash:
+      "0x2c2635a593af3801dd1fbaf559bf3d114a69b7b845eedfaf0201374559471b14",
+    amount: 0.1,
+  },
+];
