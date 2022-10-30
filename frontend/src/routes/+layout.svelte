410ab610cec4f7a6cb1d80739232b712f98ef86b
Paula Carneiro <paulacarneiro215@gmail.com>
2022-10-30 01:18:39 +0000

    Add app head

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -4,7 +4,42 @@
   import "@fontsource/ibm-plex-mono/500.css";
   import "@fontsource/ibm-plex-mono/400.css";
   import "src/assets/app.css";
+
+  import image from "src/assets/Social.png";
 </script>
+
+<svelte:head>
+  <meta charset="utf-8" />
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1.0"
+  />
+  <title>Matusalem - Crypto pension fund</title>
+  <link
+    rel="icon"
+    href="/assets/favicon.svg"
+  />
+  <meta
+    name="description"
+    content="Crypto pension fund"
+  />
+  <meta
+    name="og:description"
+    content="Crypto pension fund"
+  />
+  <meta
+    name="keywords"
+    content="crypto, ethereum, metamask, starknet, pension fund"
+  />
+  <meta
+    name="og:image"
+    content={image}
+  />
+  <meta
+    name="twitter:image"
+    content={image}
+  />
+</svelte:head>
 
 <div class="bg-beige h-full antialiased">
   <slot />
